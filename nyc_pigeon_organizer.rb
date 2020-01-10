215973a3c86df8856f7e830e72735ca688fe6bbf
Ruari Day <ruariday@gmail.com>
2020-01-10 15:33:35 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,16 @@
+
 def nyc_pigeon_organizer(data)
   # write your code here!
+  hash = {}
+  data.each do |k, v|
+    v.each do |k2, v2|
+      v2.each do |i|
+        hash[i] ||= {}
+        hash[i][k] ||= []
+        hash[i][k] << k2.to_s
+      end
+    end
+  end
+  hash
 end
+
